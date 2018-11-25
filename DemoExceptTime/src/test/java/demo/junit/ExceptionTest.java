d20aa5ea1a2d051224312f33825b3bc78bd944a4
Utilisateur <Utilisateur@Chaton.home>
2018-11-25 12:38:20 +0100

    add timeout Excpeted

--- DemoExceptTime/src/test/java/demo/junit/ExceptionTest.java
+++ DemoExceptTime/src/test/java/demo/junit/ExceptionTest.java
@@ -1,9 +1,12 @@
 package demo.junit;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.ThrowingSupplier;
 
 public class ExceptionTest {
 
@@ -18,4 +21,17 @@ public class ExceptionTest {
 
 	}
 
+	@Test
+	@DisplayName("Test Exception ant TimeOut")
+	@Tag("v2")
+	public void testException2() {
+		NullPointerException npe = Assertions.assertTimeout(Duration.ofMinutes(2),
+				(ThrowingSupplier<NullPointerException>) () -> {
+					return Assertions.assertThrows(NullPointerException.class, () -> {
+						throw new NullPointerException("toto");
+					});
+				});
+		Assertions.assertEquals("toto", npe.getMessage());
+
+	}
 }
