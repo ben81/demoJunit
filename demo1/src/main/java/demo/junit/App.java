a5ea98aeb35e1a13e0e8c70993d37b525a6657f5
Utilisateur <Utilisateur@Chaton>
2022-02-07 20:18:57 +0100

    udapte demo

--- demo1/src/main/java/demo/junit/App.java
+++ demo1/src/main/java/demo/junit/App.java
@@ -1,13 +1,34 @@
 package demo.junit;
 
+import java.util.function.Consumer;
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	public static class TestMe{
+		
+		
+		public void run() {
+			
+		}
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        TestMe t = new TestMe();
+        test((Consumer<TestMe> ) ((t0) ->t0.run()) );
+        
+        
     }
+
+	public static <T extends Object> void test(Consumer<T> object) {
+		System.out.println(object.getClass());
+		Class clazz= object.getClass();
+		System.out.println( clazz.getDeclaredMethods());
+		
+	}
 }
